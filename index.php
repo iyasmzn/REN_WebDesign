23d05adb7f93cf1fa09f317f4a561cb1e98130b4
iyasmzn <iyasmzn07@gmail.com>
2020-01-27 06:45:37 +0700

    first add

---
+++ index.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- My Style CSS -->
+	<link rel="stylesheet" type="text/css" href="assets/style.css">
+	<!-- Font Awesome 5 -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" integrity="sha256-+N4/V/SbAFiW1MPBCXnfnP9QSN3+Keu+NlB+0ev/YKQ=" crossorigin="anonymous" />
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" integrity="sha256-qM7QTJSlvtPSxVRjVWNM2OfTAz/3k5ovHOKmKXuYMO4=" crossorigin="anonymous"></script>
+</head>
+<body>
+	<div id="header-sosmed">
+		<div class="flex-between color-blue container">
+			<div class="flex-row-center fa-xs" style="font-weight: bold">
+				<div style="padding-right: 25px">
+					<a class="color-blue hover-c-green" href="#header-sosmed"><i class="far fa-envelope"></i>&nbsp&nbsp&nbsp info@reen.com</a>
+				</div>
+				<div>
+					<a class="color-blue hover-c-green" href="#header-sosmed"><i class="fas fa-mobile-alt"></i>&nbsp&nbsp&nbsp +00 (123) 456 78 90</a>
+				</div>
+			</div>
+			<div class="flex-row-center fa-xs">
+				<a class="flex-row-center w-25 fa-sm color-blue hover-bg-fb"><i class="fab fa-facebook-f"></i></a>
+				<a class="flex-row-center w-25 fa-sm color-blue hover-bg-google"><i class="fab fa-google-plus-g"></i></a>
+				<a class="flex-row-center w-25 fa-sm color-blue hover-bg-twitter"><i class="fab fa-twitter"></i></a>
+				<a class="flex-row-center w-25 fa-sm color-blue hover-bg-pin"><i class="fab fa-pinterest-p"></i></a>
+				<a class="flex-row-center w-25 fa-sm color-blue hover-bg-behance"><i class="fab fa-behance"></i></a>
+				<a class="flex-row-center w-25 fa-sm color-blue hover-bg-drib"><i class="fab fa-dribbble"></i></a>
+			</div>
+		</div>   
+	</div>
+	<div id="navbar">
+		<div class="menu-box container flex-row py-30">
+			<div>
+				<a href="#"><img class="hover-logo" src="assets/images/logo.png"></a>
+			</div>
+			<div class="menu-link flex-row">
+				<div class="menu-hover mennu">
+					<p class="color-blue -bold a-full" href="#">HOME</p>
+					<ul class="menu-dropdown">
+						<li><a class="f-size-sm-bold color-muted menu-hover-drop" href="#">AGENCY STYLE</a></li>
+						<li><a class="f-size-sm-bold color-muted menu-hover-drop" href="#">PRODUCT STYLE</a></li>
+						<li><a class="f-size-sm-bold color-muted menu-hover-drop" href="#">SERVICE STYLE</a></li>
+						<li><a class="f-size-sm-bold color-muted menu-hover-drop" href="#">PORTOFOLIO STYLE</a></li>
+						<li><a class="f-size-sm-bold color-muted menu-hover-drop" href="#">SHOWCASE STYLE</a></li>
+					</ul>
+				</div>
+				<div class=""><a class="menu-hover color-blue -bold hover-c-green" href="#">PORTOFOLIO</a></div>
+				<div><a class="menu-hover color-blue -bold hover-c-green" href="#">BLOG</a></div>
+				<div><a class="menu-hover color-blue -bold hover-c-green" href="#">PAGES</a></div>
+				<div><a class="menu-hover color-blue -bold hover-c-green" href="#">FEATURES</a></div>
+				<div><a class="menu-hover color-blue -bold hover-c-green" href="#">MEGA MENU</a></div>
+				<div><a class="menu-hover color-blue -bold hover-c-green" href="#">CONTACT</a></div>
+			</div>
+			<div>
+				<div class="flex-row-center">
+					<a class="color-blue hover-c-green" href="#"><i class="fas fa-search"></i></a>					
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="header-title">
+		<div class="container flex-row-center" style="text-align: center;height: 100%;">
+			<div style="width: 800px">
+				<h1>MADE FOR DESIGNERS</h1>
+				<h3>Create your online portofolio in munites with the professionally and lovingly designed REEN website template. Customize your site with versalite and easy to use features.</h3>
+				<a class="btn-1 btn-hover" href="#">GET STARTED NOW</a>
+			</div>
+		</div>
+	</div>
+	<div id="main-1">
+		<div class="container color-blue" style="display: flex;justify-content: center;">
+			<div style="width: 720px;text-align: center;">
+				<h2 class="f-size-xlg-bold">Beautiful.Clean.Responsive.</h2>
+				<h3 class="f-size-md-bold" class="-bold" style="padding-top: 25px;color: #8496a8;">REEN is high-quality solution for those who want a beautiful website in no time.<br> It's fully responsive and will adapt itself to any mobile device. iPad, iPhone, Android, it does'nt matter. Your content will always looks its absolute best.</h3>
+			</div>
+		</div>
+		<div class="container flex-around color-blue" style="padding-top: 50px;flex-flow: row wrap;">
+			<div class="flex-row main-1-text" >
+				<span class="pr-20 w-50"><img src="assets/images/icon-heart-empty-1.png"></span>
+				<span></span>
+				<span><h1 class="f-size-lg-bold pb-20">Passion</h1><p>Magnis modipsae que lib voloratati andigen daepeditem quiate re porem aut labor. Laceaque quiae sitiorem rest non restibusaes maio es dem tumquam core posae volor remped modis volor.</p></span>
+			</div>
+			<div class="flex-row main-1-text">
+				<span class="pr-20 w-50"><img src="assets/images/icon-lamp.png"></span>
+				<span><h1 class="f-size-lg-bold pb-20">Creativity</h1><p>Magnis modipsae que lib voloratati andigen daepeditem quiate re porem aut labor. Laceaque quiae sitiorem rest non restibusaes maio es dem tumquam core posae volor remped modis volor.</p></span>
+			</div>
+			<div class="flex-row main-1-text">
+				<span class="pr-20 w-50"><img src="assets/images/icon-star-empty-1.png"></span>
+				<span><h1 class="f-size-lg-bold pb-20">Quality</h1><p>Magnis modipsae que lib voloratati andigen daepeditem quiate re porem aut labor. Laceaque quiae sitiorem rest non restibusaes maio es dem tumquam core posae volor remped modis volor.</p></span>
+			</div>
+		</div>
+	</div>
+	<div id="main-2">
+		<div class="container">
+			<div class="pb-50" style="text-align: center;">
+				<h1 class="f-size-xlg-bold color-blue pb-20">Check out our latest works</h1>
+				<h3 class="f-size-md-bold pb-20 cwt" style="color: #8496a8;">Magnis modipsae que voloratati andigen daepeditem quiate re porem aut labor.</h3>
+			</div>
+			<div class="flex-row flex-between pb-50">
+				<div class="latest-card main-bg-hover" style="">
+					<div class="main-2-bg-1"><div class="hover-gallery">
+						<div class="hover-bg-plus flex"><i class="fas fa-plus f-size-md cwt"></i></div>
+					</div></div>
+					<div class="py-20" style="text-align: center;">
+						<h3 class="f-size-md-bold color-blue" style="padding-bottom: 10px;transition: all 0.3s;">Grand Motel gets an identity brushup</h3>
+						<h5 class="f-size-sm-bold" style="color: #8496a8;">Identity</h5>
+					</div>
+				</div>
+				<div class="latest-card main-bg-hover" style="">
+					<div class="main-2-bg-2"><div class="hover-gallery">
+						<div class="hover-bg-plus flex"><i class="fas fa-plus f-size-md cwt"></i></div>
+					</div></div>
+					<div class="py-20" style="text-align: center;">
+						<h3 class="f-size-md-bold color-blue" style="padding-bottom: 10px;transition: all 0.3s;">Fresh branding for a creative startup</h3>
+						<h5 class="f-size-sm-bold" style="color: #8496a8;">Identity</h5>
+					</div>
+				</div>
+			</div>
+			<div class="flex-row flex-between">
+				<div class="carousel-car-1"><div class="hover-gallery">
+					<div class="hover-bg-plus flex cwt"><div>
+						<h3 class="f-size-md-bold pb-10">Simple Nota</h3>
+						<p class="f-size-sm">Identity</p>
+					</div></div>
+				</div></div>
+				<div class="carousel-car-2"><div class="hover-gallery">
+					<div class="hover-bg-plus flex cwt"><div>
+						<h3 class="f-size-md-bold pb-10">Vinyl Record</h3>
+						<p class="f-size-sm">Identity</p>
+					</div></div>
+				</div></div>
+				<div class="carousel-car-3"><div class="hover-gallery">
+					<div class="hover-bg-plus flex cwt"><div>
+						<h3 class="f-size-md-bold pb-10">Astor & Yancy</h3>
+						<p class="f-size-sm">Identity</p>
+					</div></div>
+				</div></div>
+				<div class="carousel-car-4"><div class="hover-gallery">
+					<div class="hover-bg-plus flex cwt"><div>
+						<h3 class="f-size-md-bold pb-10">Signwall</h3>
+						<p class="f-size-sm">Identity</p>
+					</div></div>
+				</div></div>
+			</div>
+		</div>
+	</div>
+	<div id="main-3">
+		<div class="container" style="text-align: center;">
+			<div>
+				<h1 class="f-size-xlg-bold color-blue pb-20">Want to work with us?</h1>
+				<h3 class="f-size-md-bold pb-50" style="color: #8496a8;line-height: 25px;">Magnis modipsae que voloratati andigen daepeditem quiate re porem aut labor.<br>Laceaque quiae sitiorem rest non restibusaes maio es dem tumquam.</h3>
+				<a class="btn-1 btn-2 btn-hover" href="#main-3">GET IN TOUCH</a>
+			</div>
+		</div>
+	</div>	
+	<div id="footer-1">
+		<div class="container flex-row flex-between" style="color: white;">
+			<div class="footer-box-1">
+				<h2 class="f-size-md-bold mb-20">WHO WE ARE</h2>
+				<a href="http://localhost/web_design/css_responsive_ust_alfin/"><img class="grayscale hover-logo" src="assets/images/logo.png" style="width: 120px;"></a>
+				<p class="f-size-nm-bold mb-20 mt-20" style="color: #8496a8;line-height: 20px;letter-spacing: -0.6px;">Magnis modipsae que voloratati andigen daepeditem quiate re porem aut labor.<br>Laceaque quiae sitiorem rest non restibusaes maio es dem tumquam.</p>
+				<a class="f-size-nm-bold hover-c-green" href="#footer-1" style="color: #8496a8">More about us <i class="fa fa-arrow-right"></i></a>
+			</div>
+			<div class="footer-box-2">
+				<h2 class="f-size-md-bold mb-20">LATEST WORKS</h2>
+				<div class="thumb-box flex-row- flex-between" style="height: auto;">
+					<div class="thumb-img-1"><div class="hover-gallery">
+						<div class="hover-bg-plus flex"><i class="fas fa-plus f-size-md cwt"></i></div>
+					</div></div>
+					<div class="thumb-img-2"><div class="hover-gallery">
+						<div class="hover-bg-plus flex"><i class="fas fa-plus f-size-md cwt"></i></div>
+					</div></div>
+					<div class="thumb-img-3"><div class="hover-gallery">
+						<div class="hover-bg-plus flex"><i class="fas fa-plus f-size-md cwt"></i></div>
+					</div></div>
+					<div class="thumb-img-4"><div class="hover-gallery">
+						<div class="hover-bg-plus flex"><i class="fas fa-plus f-size-md cwt"></i></div>
+					</div></div>
+				</div>
+			</div>
+			<div class="footer-box-3">
+				<h2 class="f-size-md-bold mb-20">GET IN TOUCH</h2>
+				<p class="f-size-nm-bold mb-20 cwt" style="line-height: 20px;letter-spacing: -0.6px;">Doloreiur quia commolu commolu ptatemp dolupta oreprerum tibusam eumenis et consent accullignis dentibea autem inisita.</p>
+				<a class="f-size-nm-bold color-blue hover-c-green cwt" style="line-height: 20px;" href="#footer-1"><i class="fas fa-map-marker-alt"></i> 84 Street, City, State 24813</a><br>
+				<a class="f-size-nm-bold color-blue hover-c-green cwt" style="line-height: 20px;" href="#footer-1"><i class="fas fa-mobile-alt"></i> +00 (123) 456 78 90</a><br>
+				<a class="f-size-nm-bold color-blue hover-c-green cwt" style="line-height: 20px;" href="#footer-1"><i class="far fa-envelope"></i> info@reen.com</a>
+			</div>
+			<div class="footer-box-4">
+				<h2 class="f-size-md-bold mb-20">FREE UPDATES</h2>
+				<p class="f-size-nm-bold mb-20 cwt" style="line-height: 20px;letter-spacing: -0.6px;">Conecus iure posae volor remped modis aut lor volor accabora incim resto explabo.</p>
+				<form action="#footer-1" method="post">
+					<div class="form flex-row-center flex-between">
+						<input class="input f-size-sm-bold" type="email" name="email"  placeholder="Enter your email address">
+						<button class="btn-1 btn-3 btn-hover f-size-sm-bold" type="submit" >SUBSCRIBE</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>	
+	<div id="footer-2">
+		<div class="container flex-between"> 
+			<div style=""><p class="f-size-nm-bold cwt">2020 REEN. All rights reserved.</p></div>
+			<div class="flex-row-center">
+				<a class="f-size-nm-bold cwt hover-c-green px-7" href="#footer-2"> Home </a>
+					<i class="fas fa-circle cwt" style="font-size: 0.2em;"></i>
+				<a class="f-size-nm-bold cwt hover-c-green px-7" href="#footer-2"> Portofolio </a>
+					<i class="fas fa-circle cwt" style="font-size: 0.2em;"></i>
+				<a class="f-size-nm-bold cwt hover-c-green px-7" href="#footer-2"> Blog </a>
+					<i class="fas fa-circle cwt" style="font-size: 0.2em;"></i>
+				<a class="f-size-nm-bold cwt hover-c-green px-7" href="#footer-2"> About </a>
+					<i class="fas fa-circle cwt" style="font-size: 0.2em;"></i>
+				<a class="f-size-nm-bold cwt hover-c-green px-7" href="#footer-2"> Services </a>
+					<i class="fas fa-circle cwt" style="font-size: 0.2em;"></i>
+				<a class="f-size-nm-bold cwt hover-c-green px-7" href="#footer-2"> Contact </a>
+				<a href="#header-title" class="arrow-up cwt hover-c-green f-size-sm"><i class="fas fa-chevron-up f-size-lg"></i></a>
+			</div>
+		</div>
+	</div>	
+	<script type="text/javascript" src="assets/script.js">
+		// $(document).ready(function() {
+		// 	$('.carousel').carousel();
+		// 	$(window).scroll(function() {
+		// 		var navbar = $('.nava').offset().top;
+		// 		var scroll = $(window).scrollTop();
+		// 		if (scroll > 100) {
+		// 			$('.nava').addClass('sticky-header').removeClass('nava-abs').removeClass('py-4');
+		// 		}
+		// 		else {
+		// 			$('.nava').removeClass('sticky-header').addClass('nava-abs').addClass('py-4');	
+		// 		}
+		// 	});
+		// });
+	</script>
+</body>
+</html>
